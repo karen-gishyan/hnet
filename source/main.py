0d36c5b532e099f9862a0d020c34905f1e6ad1e7
Karen Gishyan <karengishyann@gmail.com>
2022-04-23 16:19:20 +0400

    updates

--- source/main.py
+++ source/main.py
@@ -1,12 +1,13 @@
 import os
 import sys
-cwd=os.getcwd()
+
+cwd = os.getcwd()
 if not cwd in sys.path:
     sys.path.append('.')
 
 from collections import deque
 from source.utils import make_train_data, get_drug_weights, merge_dfs
-from source.search import Graph, store_graph, find_path
+from source.search import Graph, store_sequence, find_path, store_graph
 
 if __name__ == "__main__":
     #### path merging algorithm
@@ -14,8 +15,9 @@ if __name__ == "__main__":
     path2 = [('A', 'C'), ('C', 'E'), ('E', 'D')]
     path3 = [('B', 'F'), ('F', 'G'), ('G', 'E'), ('E', 'F')]
 
+    paths = [i for i in (path1, path2, path3)]
     average_path_length = round((len(path1) + len(path2) + len(path3)) / 3)
-    start_edges, adjacency_matrix = store_graph(path1, path1, path3)
+    start_edges, adjacency_matrix = store_sequence(paths)
     max_start_edge = max(start_edges, key=lambda _key: start_edges[_key])
     path_list = []
     que = deque()
@@ -23,8 +25,10 @@ if __name__ == "__main__":
     path_list.append(max_start_edge[1])
     find_path(que, adjacency_matrix, average_path_length, path_list)
 
-    #### A Star logic
+    ##########
     df = merge_dfs(drug_name='PNEUMONIA')
     graph = Graph(df)
-    paths=graph.search_space()
-
+    sequenc_list, edge_list = graph.search_space()
+    adjacency_df, start_adjacency_df, edges_cost_sum, start_edges_cost_sum = store_graph(edge_list)
+    adjacency_df.to_csv('data/adjacency_df.csv')
+    start_adjacency_df.to_csv('data/start_adjacency_df.csv')
--- source/search.py
+++ source/search.py
@@ -1,10 +1,8 @@
-import json
-import os
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 from source.utils import calculate_date_intersection
-from collections import Counter
+from collections import Counter, defaultdict
 from itertools import chain
 import math
 
@@ -20,11 +18,14 @@ class Graph():
          Construct edges between drug nodes based on the fact if two drugs are used in combination.
          Combination means drugs are applied in the same timeframe.
          """
-        path_list = []
+        path_list = []  # total list of edges from each admission
+        sequence_list = []  # drugs as sequences, duplicates removed
         for adm_i, admission in enumerate(self.unique_admissions):
             patient_df = self.df[self.df.hadm_id == admission]
             patient_df.reset_index(inplace=True)
-            patient_df_edges = []
+            # TODO set is for removing self loops, which may result in information loss
+            sequence_list.append(list(set(patient_df.drug)))  # append unique drugs as a sequence list
+            patient_df_edges = {}
             n_drugs = patient_df.shape[0]
             discharge_location = list(patient_df.discharge_location)[0]
             for i, row_i in patient_df.iterrows():
@@ -40,20 +41,21 @@ class Graph():
                 else:
                     relative_cost = patient_stay_length
                 child1_cost = self.cost_function(relative_cost, n_drugs, discharge_location)
-                patient_df_edges.append((row_i.drug, child_row_1.drug, child1_cost))
+                patient_df_edges.update({(row_i.drug, child_row_1.drug): child1_cost})
                 if child_node_2_days_intersection:
                     relative_cost = round(patient_stay_length / child_node_2_days_intersection) + 1
                 else:
                     # +1 cost for being not the directly connected node in the sequence
                     relative_cost = patient_stay_length + 1
                 child2_cost = self.cost_function(relative_cost, n_drugs, discharge_location)
-                patient_df_edges.append((row_i.drug, child_row_2.drug, child2_cost))
+                patient_df_edges.update({(row_i.drug, child_row_2.drug): child2_cost})
 
             path_list.append(patient_df_edges)
             print(f"Iter {adm_i} completed")
+            if adm_i == 10:
+                break
         # print(nx.is_tree(self.graph))
-        return path_list
-
+        return sequence_list, path_list
 
     def cost_function(self, days_intersection_cost: int, path_lengh: int, discharge_location: str):
         """
@@ -67,16 +69,6 @@ class Graph():
             return days_intersection_cost + math.log2(path_lengh) + 1  # fixed penalty
         return round(days_intersection_cost + math.log2(path_lengh))
 
-    #TODO should be removed
-    def consistent_heuristic(self, parent_node, child_node, parent_to_child_cost):
-
-        with open('jsons/heuristics.json', 'r') as file:
-            heuristics_dict = json.load(file)
-        heuristics_parent = heuristics_dict.get(parent_node)
-        heuristics_child = heuristics_dict.get(child_node)
-        heuristics_parent = min(heuristics_parent, parent_to_child_cost + heuristics_child)
-        return heuristics_parent
-
     def visualize(self):
         position = nx.spring_layout(self.graph)
         nx.draw(self.graph, position, with_labels=True)
@@ -86,16 +78,17 @@ class Graph():
         plt.show()
 
 
-def store_graph(*args):
+def store_sequence(path_list2d):
     """
-    each arg is a list of tuples, where each tuple represents an edge between nodes.
+    Parameters:
+        path_list2d: list of edges from each sequence, result of Graph().search_space()
     """
     # store starting edges from each path
-    start_edges = sorted([tuple_ for path in args for (i, tuple_) in enumerate(path) if i == 0])
+    # for path in args, iterate over the path and store the first one
+    start_edges = sorted([tuple_ for path in list(path_list2d) for (i, tuple_) in enumerate(path) if i == 0])
     # TODO if single listed is passed, no need to combine.
-
     # unpack the paths and chain them into a sorted list.
-    path_combine = sorted(list(chain(*args)))
+    path_combine = sorted(list(chain(*path_list2d)))
     # count the frequency of edges among start edges and total path edges
     start_edges_counter = Counter(start_edges)
     path_combine_counter = Counter(path_combine)
@@ -107,6 +100,48 @@ def store_graph(*args):
     for key, value in path_combine_counter.items():
         adjacency_df.at[key[0], key[1]] = value
     return start_edges_counter, adjacency_df
+
+
+def store_graph(list2d):
+    """
+     Parameters:
+         path_list2d: list of dictionaries, where each dict stores edge pairs for each admission.
+     """
+
+    # start_edges = sorted([tuple_ for dict_ in list(list2d) for (i, tuple_) in enumerate(dict_.keys()) if i == 0])
+    start_edges_frequency = defaultdict(int)
+    start_edges_cost_sum = defaultdict(int)
+    edges_frequency = defaultdict(int)
+    edges_cost_sum = defaultdict(int)
+    start_node_list, node_list = [], []
+    for dict_ in list2d:
+        for i, tuple_ in enumerate(dict_.keys()):
+            # append each drug, uniqueness will be sorted later with set
+            node_list.append(tuple_[0])
+            node_list.append(tuple_[1])
+            if i == 0:
+                start_node_list.append(tuple_[0])
+                start_node_list.append(tuple_[1])
+                start_edges_frequency[tuple_] += 1
+                start_edges_cost_sum[tuple_] += dict_[tuple_]
+            # TODO think if this needs to be part of else
+            edges_frequency[tuple_] += 1
+            edges_cost_sum[tuple_] += dict_[tuple_]
+
+    node_list = sorted(set(node_list))
+    start_node_list = sorted(set(start_node_list))
+
+    adjacency_df = pd.DataFrame(index=node_list, columns=node_list)
+    adjacency_df.fillna(0, inplace=True)
+    start_adjacency_df = pd.DataFrame(index=start_node_list, columns=start_node_list)
+    start_adjacency_df.fillna(0, inplace=True)
+
+    for key, value in edges_frequency.items():
+        adjacency_df.at[key[0], key[1]] = value
+    for key, value in start_edges_frequency.items():
+        start_adjacency_df.at[key[0], key[1]] = value
+
+    return adjacency_df, start_adjacency_df, edges_cost_sum, start_edges_cost_sum
 
 
 def find_path(que, adjacency_matrix, average_path_length, path_list):
